4de81a188762d164d69491804473d287161b0313
puneetsreevatsalj <puneetsreevatsalj@gmail.com>
2026-03-09 15:44:28 +0530

    Puneet Added UC7-DequeueBasedPalindromeCheck

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,64 +1,28 @@
-public class UseCase8PalindromeCheckerApp {
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
 
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
+public class UseCase6PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String input = "madam";
 
-        Node head = null;
-        Node tail = null;
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
 
         for (char c : input.toCharArray()) {
-            Node newNode = new Node(c);
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
+            queue.add(c);
+            stack.push(c);
         }
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node prev = null;
-        Node current = slow;
-        Node next = null;
-
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
-        }
-
-        Node firstHalf = head;
-        Node secondHalf = prev;
 
         boolean isPalindrome = true;
 
-        while (secondHalf != null) {
-            if (firstHalf.data != secondHalf.data) {
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            firstHalf = firstHalf.next;
-            secondHalf = secondHalf.next;
         }
 
         if (isPalindrome) {
@@ -66,4 +30,5 @@ public class UseCase8PalindromeCheckerApp {
         } else {
             System.out.println(input + " is NOT a Palindrome");
         }
-
+    }
+}
